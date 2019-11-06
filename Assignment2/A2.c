89172d406714abea7883f75b8035ef0af4a525e5
Nicholas Rahbany <rahb3032@kettering.edu>
2019-11-06 17:12:31 -0500

    Saving Progress

--- Assignment2/A2.c
+++ Assignment2/A2.c
@@ -67,9 +67,17 @@ int **fileToArray(char *fileName)
     return fileArray;
 }
 
+void printWaitQueue()
+{
+    for (int i = 0; i < fileLines; i++)
+    {
+        printf("Wait list[%d]: %d\n", i, waitList[i]);
+    }
+}
+
 void startProcess(int processID)
 {
-    if (currProcess != processID && currProcess != -1)
+    if (currProcess != processID)
     {
         printf("Stopping process: %d\n", currProcess);
     }
@@ -83,7 +91,21 @@ int comparePriority(int pid1, int pid2)
     {
         return fileData[pid1][0];
     }
-    return fileData[pid2][0];
+    else if (fileData[pid1][3] > fileData[pid2][3])
+    {
+        return fileData[pid2][0];
+    }
+    else
+    {
+        if (fileData[pid1][1] > fileData[pid2][1])
+        {
+            return pid2;
+        }
+        else
+        {
+            return pid1;
+        }
+    }
 }
 
 void updateWaitListArray(int pid1, int pid2, int arrayIndex)
@@ -91,32 +113,39 @@ void updateWaitListArray(int pid1, int pid2, int arrayIndex)
     if (pid1 == -1)
     {
         waitList[arrayIndex] = pid2;
-        printf("Added %d to slot %d\n", pid2, arrayIndex);
+        // printf("Added %d to slot %d\n", pid2, arrayIndex);
     }
     else if (comparePriority(pid1, pid2) == pid2)
     {
+        // Pid2 has higher priority than pid1
         waitList[arrayIndex] = pid2;
-        printf("Added %d to slot %d\n", pid2, arrayIndex);
+        // printf("Added %d to slot %d\n", pid2, arrayIndex);
         updateWaitListArray(waitList[arrayIndex + 1], pid1, arrayIndex + 1);
     }
     else
     {
+        //pid1 has higher priority over pid2
         updateWaitListArray(waitList[arrayIndex + 1], pid2, arrayIndex + 1);
-    }
-}
-
-void printWaitQueue()
-{
-    for (int i = 0; i < fileLines; i++)
-    {
-        printf("Wait list[%d]: %d\n", i, waitList[i]);
     }
 }
 
 void addToQueue(int process)
 {
+    printf("Adding %d to waitlist\n", process);
     updateWaitListArray(waitList[0], process, 0);
-    printWaitQueue();
+}
+
+int processNotInWait(int process)
+{
+    int result = 1;
+    for (int i = 0; i < fileLines; i++)
+    {
+        if (waitList[i] == process)
+        {
+            result = 0;
+        }
+    }
+    return result;
 }
 
 int getPriorityProcess()
@@ -128,10 +157,21 @@ int getPriorityProcess()
         {
             if (fileData[i][1] == timePassed)
             {
-                if (fileData[priorityProcess][3] > fileData[i][3])
+                if (fileData[priorityProcess][3] >= fileData[i][3] || processNotInWait(fileData[i][0]) == 1)
                 {
-                    addToQueue(fileData[priorityProcess][0]);
-                    priorityProcess = fileData[i][0];
+                    if (comparePriority(fileData[priorityProcess][0], fileData[i][0]) == fileData[i][0])
+                    {
+                        addToQueue(fileData[priorityProcess][0]);
+                        priorityProcess = fileData[i][0];
+                    }
+                    else
+                    {
+                        addToQueue(fileData[i][0]);
+                    }
+                }
+                else
+                {
+                    addToQueue(fileData[i][0]);
                 }
             }
         }
@@ -143,15 +183,57 @@ int getPriorityProcess()
     return priorityProcess;
 }
 
+void shiftWaitList()
+{
+    for (int i = 0; i < fileLines; i++)
+    {
+        if (waitList[i] != -1)
+        {
+            waitList[i] = waitList[i + 1];
+        }
+    }
+}
+
+void terminateProcess()
+{
+    //Send termiante signal to process
+    currProcess = waitList[0];
+    shiftWaitList();
+}
+
+void tickCurrProcess()
+{
+    int terminate = 0;
+    for (int i = 0; i < fileLines; i++)
+    {
+        if (fileData[i][0] == currProcess && terminate == 0)
+        {
+            int time = fileData[i][2] - 1;
+            fileData[i][2] = time;
+            if (time == 0)
+            {
+                terminateProcess();
+                terminate = 1;
+            }
+        }
+        printf("Time[%d]: %d\n", i, fileData[i][2]);
+    }
+}
+
 void checkCurrentProcess()
 {
-    ++timePassed;
     int priorityProcess = getPriorityProcess();
     startProcess(priorityProcess);
 }
 
 void timer_handler(int signal)
 {
+    timePassed++;
+    printf("%d Seconds\n", timePassed);
+    if (currProcess != -1)
+    {
+        tickCurrProcess();
+    }
     checkCurrentProcess();
 }
 
--- Assignment2/prime.c
+++ Assignment2/prime.c
@@ -4,9 +4,66 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
+long unsigned int calculatedPrime = 123400003;
+void handler(int signum);
+int checkPrimeAndPrint(unsigned long int toCheck);
+
+int main(int argc, char **argv)
 {
-    printf("Prime C");
+
+    struct sigaction sa_suspend;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = suspend_handler;
+    sigaction(SIGTSTP, &sa, NULL);
+
+    struct sigaction sa_continue;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = continue_handler;
+    sigaction(SIGCONT, &sa, NULL);
+
+    struct sigaction sa_terminate;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = terminate_handler;
+    sigaction(SIGQUIT, &sa, NULL);
+
+    int numPrinted = 0;
+    long unsigned int numberToCheck = calculatedPrime + 1;
+    while (numPrinted < 10)
+    {
+        if (checkPrimeAndPrint(numberToCheck) == 1)
+        {
+            printf("prime number is %lu \n", numberToCheck);
+            numPrinted++;
+        }
+        numberToCheck++;
+    }
+}
+
+void suspend_handler(int signum)
+{
+    // Write handler code here.
+}
+
+void continue_handler(int signum)
+{
+    // Write handler code here.
+}
+
+void terminate_handler(int signum)
+{
+    // Write handler code here.
+}
+
+int checkPrimeAndPrint(unsigned long int toCheck)
+{
+    unsigned long int i = 2;
+    int prime = 1;
+    while (prime == 1 && i < toCheck / 2)
+    {
+        if (toCheck % i == 0)
+            prime = 0;
+        i++;
+    }
+    return (prime);
 }
